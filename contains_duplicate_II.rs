fc78d1f73f544e2d0c9a5fb7fb499a28370c8037
Kirubel Midru <kirubelmidru@gmail.com>
2025-01-29 18:54:45 +0300

    Create contains_duplicate_II.rs

---
+++ contains_duplicate_II.rs
@@ -0,0 +1,18 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        let mut k = k as usize;
+        let mut hash: HashSet<i32> = HashSet::new();
+
+        for (i, &v) in nums.iter().enumerate() {
+            if !hash.insert(v) {
+                return true;
+            }
+            if i + 1 > k {
+                hash.remove(&nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
